446ce2c8bdd957cd4e94e2aecb669dcffb79fa1a
jinwon7878 <loveyjw78@gmail.com>
2023-06-25 18:02:29 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// 로그인 팝업창 띄우기 (보다 로그인창이 나을 것 같아서 시도X) 
+
+//scroll animation(newhome.html)
+// let mainText = document.querySelectorAll('.text');
+// window.addEventListener('scroll', function(){
+//     let value = window.scrollY;
+
+
+// })
